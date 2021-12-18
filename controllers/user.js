e457c5198b0d5b479becedfc302ff2d9704ffd91
miguelangelvegaalonso <miguelangel.vegaalonso@mercadolibre.com.co>
2021-12-17 22:35:27 -0500

    fix find user

--- controllers/user.js
+++ controllers/user.js
@@ -5,12 +5,13 @@ module.exports = {
     newUser: async (req, res, next) => {
         const newUser = new User(req.body);
         const user = await newUser.save();
-        res.status(200).json(user);
+        res.status(201).json(user);
     },
 
     getUser: async (req, res, next) => {
         const { userId } = req.params;
-        const user = await User.findById(userId);
+        // const user = await User.findById(userId);
+        const user = await User.find({ numeroDocumento: userId}).exec();
         res.status(200).json(user);
     },
 
